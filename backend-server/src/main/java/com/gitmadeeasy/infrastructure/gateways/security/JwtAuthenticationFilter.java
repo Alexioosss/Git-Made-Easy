2fe495343dd4e3e775510d7e6058664d2cbcacd4
Alessio <alessiococuzza2@gmail.com>
2026-02-12 12:40:20 +0000

    Made the authentication filter throw a custom error message instead of spring's default

--- backend-server/src/main/java/com/gitmadeeasy/infrastructure/gateways/security/JwtAuthenticationFilter.java
+++ backend-server/src/main/java/com/gitmadeeasy/infrastructure/gateways/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -42,6 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 }
             } catch(JwtException | IllegalArgumentException e) {
                 SecurityContextHolder.clearContext();
+                throw new BadCredentialsException("Invalid or expired JWT Token");
             }
         }
         filterChain.doFilter(request, response);
