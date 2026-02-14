a87ae0fd48f6e43a82b66e8cd1031f4c38b44a1c
Alessio <alessiococuzza2@gmail.com>
2026-02-14 23:54:28 +0000

    Further changes to make application.properties work with CI

--- backend-server/src/test/java/com/gitmadeeasy/integration/controllers/TaskProgressControllerIntegrationTest.java
+++ backend-server/src/test/java/com/gitmadeeasy/integration/controllers/TaskProgressControllerIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class TaskProgressControllerIntegrationTest {
     @Autowired private MockMvc mockMvc;
     @Autowired private JpaTaskAttemptRepository taskAttemptRepository;
