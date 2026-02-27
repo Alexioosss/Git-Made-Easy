b79366cb4e2d392253c6bbaaae77a31fcada1279
Alessio <alessiococuzza2@gmail.com>
2026-02-27 10:50:50 +0000

    Renamed the frontend environment variable for backend url

--- frontend-app/infrastructure/factories/apiClientFactory.ts
+++ frontend-app/infrastructure/factories/apiClientFactory.ts
@@ -2,7 +2,7 @@ import { FetchApiClient } from "../gateways/FetchApiClient";
 
 export class ApiClientFactory {
     static createApiClient() {
-        const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+        const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080';
         return new FetchApiClient(BASE_URL);
     }
 }
