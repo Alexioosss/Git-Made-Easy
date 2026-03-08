cf3d7d27d1dd2e034d0a0d167088df9f5ea35081
Alessio <alessiococuzza2@gmail.com>
2026-03-08 22:19:42 +0000

    Edited firebase configuration file to read from both env variable and resource path

--- backend-server/src/main/java/com/gitmadeeasy/infrastructure/configs/firebase/FirebaseConfig.java
+++ backend-server/src/main/java/com/gitmadeeasy/infrastructure/configs/firebase/FirebaseConfig.java
@@ -9,22 +9,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration @Profile("!test")
 public class FirebaseConfig {
     @Value("${firebase.service-account}")
     private Resource serviceAccount;
 
+    @Value("#{systemEnvironment['FIREBASE_SERVICE_ACCOUNT_JSON']}")
+    private String environmentVariable;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        try(InputStream stream = this.serviceAccount.getInputStream()) {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(stream)).build();
-
-            if(FirebaseApp.getApps().isEmpty()) { return FirebaseApp.initializeApp(options); }
-            else { return FirebaseApp.getInstance(); }
+        InputStream stream;
+        if(environmentVariable != null && !environmentVariable.isEmpty()) {
+            stream = new ByteArrayInputStream(environmentVariable.getBytes(StandardCharsets.UTF_8));
         }
+        else if (serviceAccount != null && serviceAccount.exists()) { stream = this.serviceAccount.getInputStream(); }
+        else {
+            throw new IllegalStateException("Firebase service account JSON not provided via resource or environment variable.");
+        }
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(stream)).build();
+        if(FirebaseApp.getApps().isEmpty()) { return FirebaseApp.initializeApp(options); }
+        else { return FirebaseApp.getInstance(); }
     }
 }
