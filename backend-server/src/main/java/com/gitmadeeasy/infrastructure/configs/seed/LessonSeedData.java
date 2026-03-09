864f03e29218a962772a7ae089828954efbaac58
Alessio <alessiococuzza2@gmail.com>
2026-03-09 20:59:37 +0000

    Made changes to the lesson and task data to make them more user friendly and helpful

--- backend-server/src/main/java/com/gitmadeeasy/infrastructure/configs/seed/LessonSeedData.java
+++ backend-server/src/main/java/com/gitmadeeasy/infrastructure/configs/seed/LessonSeedData.java
@@ -14,13 +14,13 @@ public class LessonSeedData {
 
         Lesson introduction = new Lesson("Introduction to Git", "Learn the basics of Git", DifficultyLevels.EASY, 1);
         introduction.setTasks(List.of(
-                new Task(introduction.getLessonId(), "Initialize a Git repository",
+                new Task(introduction.getLessonId(), "Initialize a Git repository using the keyword init",
                         "Run the command to initialise Git", "git init",
                         "Use git inside your project folder", 1, DifficultyLevels.EASY),
-                new Task(introduction.getLessonId(), "Check the current Git version", "Check your installed Git version",
+                new Task(introduction.getLessonId(), "Check the current Git version", "Check your installed Git version using --version",
                         "git --version", "Try running --version in your terminal, and the Git version should show",
                         2, DifficultyLevels.EASY),
-                new Task(introduction.getLessonId(), "View repository status", "See what has changed so far",
+                new Task(introduction.getLessonId(), "View repository status", "Check the status of the repository. See what has changed so far",
                         "git status", "Use git status to view latest changes", 3, DifficultyLevels.EASY)
         ));
         introduction.setLongDescription("""
@@ -35,17 +35,17 @@ public class LessonSeedData {
         Lesson filesAndCommits = new Lesson("Working with Files & Commits",
                 "Learn how to stage, commit, and track file changes.", DifficultyLevels.EASY, 2);
         filesAndCommits.setTasks(List.of(
-                new Task(filesAndCommits.getLessonId(), "Stage a file", "Add a file to the staging area",
+                new Task(filesAndCommits.getLessonId(), "Stage a file", "Let's add a file to the staging area, for this case, example.txt will do.",
                         "git add example.txt",
-                        "Use git add <file>. And don't forget the file extension type." +
+                        "Use git add <file>. And don't forget the file extension type. " +
                                 "For this case, replace <file> with example.txt", 1, DifficultyLevels.EASY),
-                new Task(filesAndCommits.getLessonId(), "Commit changes", "Create a commit with a message",
+                new Task(filesAndCommits.getLessonId(), "Commit changes", "Create a commit with a message. Let's say 'Added first file'",
                         "git commit -m 'Added first file'",
-                        "Commit messages should be meaningful and descriptive. Let's give it a descriptive message." +
+                        "Commit messages should be meaningful and descriptive. Let's give it a descriptive message. " +
                                 "Let's say: 'Added first file'", 2, DifficultyLevels.EASY),
-                new Task(filesAndCommits.getLessonId(), "Stage all changes", "Stage all modified files",
+                new Task(filesAndCommits.getLessonId(), "Stage all changes", "Stage all modified files by adding them all to the staging index. The '.' includes all modified files.",
                         "git add .", "Be careful when using add .", 3, DifficultyLevels.EASY),
-                new Task(filesAndCommits.getLessonId(), "View commit history", "See previous commits",
+                new Task(filesAndCommits.getLessonId(), "View commit history", "See a log of previous commits made within the repository",
                         "git log", "Use the command that shows a list, or log, of all past commits",
                         4, DifficultyLevels.EASY)
         ));
@@ -63,20 +63,20 @@ public class LessonSeedData {
                 new Task(branchingBasics.getLessonId(), "Create a new branch", "Create a new branch for your own work. " +
                         "Let's call it my-new-branch", "git branch my-new-branch",
                         "Specify the branch name after the keyword branch", 1, DifficultyLevels.EASY),
-                new Task(branchingBasics.getLessonId(), "Switch to a branch", "Move to the branch you just created",
+                new Task(branchingBasics.getLessonId(), "Switch to a branch", "You will need the Git command to check out the branch you just created, called my-new-branch",
                         "git checkout my-new-branch",
-                        "The branch is called my-new-branch", 2, DifficultyLevels.MEDIUM),
+                        "The branch is called my-new-branch, don't forget the key word checkout", 2, DifficultyLevels.MEDIUM),
                 new Task(branchingBasics.getLessonId(), "Create and switch in one step",
                         "Create a new branch and switch to it immediately",
                         "git checkout -b my-new-branch",
                         "Think of combining branch creation and switching into a single command." +
                                 "The switching command appears first. Branch is called my-new-branch", 3, DifficultyLevels.MEDIUM),
                 new Task(branchingBasics.getLessonId(), "List all branches", "View all branches in your repository",
-                        "git branch -a", "Use the command that shows ALL branches and highlights the current one.",
+                        "git branch -a", "Use the branch command with the flag that shows ALL branches and highlights the current one.",
                         4, DifficultyLevels.MEDIUM),
                 new Task(branchingBasics.getLessonId(), "Delete a branch", "Remove a branch you no longer need",
                         "git branch -d my-new-branch",
-                        "Use the branch command with the delete flag to remove a branch safely." +
+                        "Use the branch command with the delete flag to remove a branch safely. " +
                                 "Let's delete the branch my-new-branch", 5, DifficultyLevels.MEDIUM)
         ));
         branchingBasics.setLongDescription("""
@@ -93,16 +93,16 @@ public class LessonSeedData {
                 "Learn how to combine changes from different branches and resolve conflicts.", DifficultyLevels.MEDIUM, 4);
         mergingAndConflicts.setTasks(List.of(
                 new Task(mergingAndConflicts.getLessonId(), "Merge a branch",
-                        "Let's just say we did not delete the previous branch, so let's combine changes from one branch into another. Let's merge into branch 'my-new-branch'",
+                        "Let's just say we did not delete the previous branch, and that we are currently in a different branch, so let's combine changes from one branch into another. Let's merge into branch 'my-new-branch'",
                         "git merge my-new-branch",
                         "Specify the branch that you want to merge into", 1, DifficultyLevels.MEDIUM),
                 new Task(mergingAndConflicts.getLessonId(), "View merge conflicts", "After merging, check the status of the repository to check if any files have conflicts that need resolving.",
                         "git status",
-                        "Files with conflicts will be marked as 'both modified'; check these before committing. Use this command to find files that need checking before completing the merge.", 2, DifficultyLevels.MEDIUM),
-                new Task(mergingAndConflicts.getLessonId(), "Resolve a conflict", "Edit the conflicted files and mark as resolved",
+                        "Files with conflicts will be marked as 'both modified'; you must check the status of these files before committing. Use this command to find files that need checking before completing the merge.", 2, DifficultyLevels.MEDIUM),
+                new Task(mergingAndConflicts.getLessonId(), "Resolve a conflict", "Edit the conflicted files and mark as resolved. Let's say we resolved conflicts locally, let's add my-new-file to the staging index.",
                         "git add my-new-file", "Once conflicting files have been resolved, " +
                                 "they are then pushed to the repository. Let's just pretend that we just fixed a file conflict... Let's stage the file 'my-new-file'", 3, DifficultyLevels.MEDIUM),
-                new Task(mergingAndConflicts.getLessonId(), "Finalize the merge", "Complete the merge after resolving conflicts",
+                new Task(mergingAndConflicts.getLessonId(), "Finalize the merge", "Complete the merge after resolving conflicts. To complete merges, a final commit is required. Git provides a commit message for you after conflicts are resolved.",
                         "git commit",
                         "If conflicts were resolved manually, Git requires a commit to finalize the merge. Recalling a previous task...", 4, DifficultyLevels.MEDIUM)
         ));
@@ -122,9 +122,9 @@ public class LessonSeedData {
                 new Task(remoteRepositories.getLessonId(), "Clone a repository",
                         "Create a local copy of a remote repository. Let's clone this remote repository: https://github.com/onaly/storage",
                         "git clone https://github.com/onaly/storage",
-                        "Clone the repository using its public URL", 1, DifficultyLevels.MEDIUM),
+                        "Clone the repository using its public URL. The URL is: https://github.com/onaly/storage", 1, DifficultyLevels.MEDIUM),
                 new Task(remoteRepositories.getLessonId(), "Push a local branch to a remote repository",
-                        "Push your local branch to the remote repository", "git push origin my-new-branch",
+                        "Push your local branch to the remote repository. The branch is my-new-branch, and the remote is origin", "git push origin my-new-branch",
                         "Let's push our local branch, my-new-branch, to the remote repository (origin). This updates the remote repository with your local changes.",
                         2, DifficultyLevels.MEDIUM)
         ));
@@ -145,11 +145,11 @@ public class LessonSeedData {
                 new Task(rebaseAndSquashing.getLessonId(), "Resolving Rebase Conflicts",
                         "Handle conflicts during rebase and continue the rebase process",
                         "git rebase --continue",
-                        "Resolve conflicts manually, stage them, then rebase and Continue." +
+                        "Resolve conflicts manually, stage them, then rebase and Continue. " +
                                 "For this case, let's just say that we already resolved said conflicts. Just rebase and continue",
                         2, DifficultyLevels.HARD),
                 new Task(rebaseAndSquashing.getLessonId(), "Force Push After Rebase",
-                        "Force push your rebased branch to remote after rewriting history",
+                        "Force push your rebased branch to the remote after rewriting history",
                         "git push --force",
                         "Be cautious with force pushing, especially on shared branches.", 3, DifficultyLevels.HARD)
         ));
